c1cf6b558b056ddf5d7b1cbc575a6a5c3871eab3
cmdrk33n <cmdrk33n@googlemail.com>
2012-04-07 11:49:29 +0200

    gplv3 added

--- ledcube_firmware/bitmaps.h
+++ ledcube_firmware/bitmaps.h
@@ -1,9 +1,28 @@
 /*
- * bitmaps.h
- *
- *  Created on: 30.03.2012
- *      Author: pueppi
- */
+===========================================================================
+
+ledcube_firm GPL Source Code
+Copyright (C) 2012 Tino Göhlert, cmdrkeen.net.
+
+This file is part of the ledcube_firm Source Code.
+
+ledcube_firm is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+ledcube_firm Source Code is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with ledcube_firm Source Code.  If not, see <http://www.gnu.org/licenses/>.
+
+===========================================================================
+*/
+
+
 /*
  * 		5x5 bitmaps packed in unsigned ints
  *
--- ledcube_firmware/ledcube.cpp
+++ ledcube_firmware/ledcube.cpp
@@ -1,9 +1,26 @@
 /*
- * ledcube.cpp
- *
- *  Created on: 20.03.2012
- *      Author: pueppi
- */
+===========================================================================
+
+ledcube_firm GPL Source Code
+Copyright (C) 2012 Tino Göhlert, cmdrkeen.net.
+
+This file is part of the ledcube_firm Source Code.
+
+ledcube_firm is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+ledcube_firm Source Code is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with ledcube_firm Source Code.  If not, see <http://www.gnu.org/licenses/>.
+
+===========================================================================
+*/
 
 #include "Arduino.h"
 #include "ledcube.h"
